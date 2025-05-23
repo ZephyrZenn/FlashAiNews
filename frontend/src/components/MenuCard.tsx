24a72d14543b84f582afaeb6d01b11d0c5cd4e16
Zephyr <1546327522@qq.com>
2025-05-23 11:21:06 +0800

    fix: card overflow

--- frontend/src/components/MenuCard.tsx
+++ frontend/src/components/MenuCard.tsx
@@ -1,7 +1,8 @@
 export default function MenuCard({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto w-full h-full pt-6 pb-6 pl-6">
-      <div className="relative h-full">
+    <div className="mx-auto w-full h-full min-h-[250px] pt-6 pb-6 pl-6">
+      {/* Card with layered shadow effect */}
+      <div className="relative">
         {/* Bottom shadow layer */}
         <div className="absolute -bottom-2 -right-2 w-full h-full bg-gray-300 rounded-xl"></div>
 
@@ -9,7 +10,7 @@ export default function MenuCard({ children }: { children: React.ReactNode }) {
         <div className="absolute -bottom-1 -right-1 w-full h-full bg-gray-200 rounded-xl"></div>
 
         {/* Main card */}
-        <div className="relative bg-neutral-50 text-gray-900 rounded-xl p-6 shadow-lg border border-gray-100 w-full h-full overflow-y-auto">
+        <div className="relative bg-neutral-50 text-gray-900 rounded-xl p-6 shadow-lg border border-gray-100 w-full h-[80vh] overflow-y-auto">
           {children}
         </div>
       </div>

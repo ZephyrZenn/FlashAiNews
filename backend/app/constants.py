3213721bed5045d423c7e27e79027e7716913bd7
Zephyr <1546327522@qq.com>
2025-05-23 11:39:10 +0800

    feat: prompt template

--- backend/app/constants.py
+++ backend/app/constants.py
@@ -3,25 +3,22 @@ import datetime
 SUMMARY_LENGTH = 500
 DEFAULT_FEED_LAST_USED_DATE = datetime.datetime(1970, 1, 1, 0, 0, 0)
 
-DEFAULT_PROMPT = """
+PROMPT_TEMPLATE = """
 ```xml
 <instruction>
 You are tasked with synthesizing multiple news articles into a single, concise summary. Follow these steps to complete the task:
 
 1. **Read the Input Articles**: You will receive a set of articles in JSON format. Each article contains a title and content. The content may include HTML tags, which you should ignore in your summary.
 
-2. **Identify Core Themes**: As you read through the articles, look for common themes, major updates, and significant points of consensus or disagreement across the articles. 
+2. **Maintain Brevity**: Your summary should be concise, ideally between 800-1000 words. Use clear and direct language to ensure it is easily digestible for a quick read.
 
-3. **Synthesize Information**: Combine the insights from all articles into one integrated summary. Focus on the bigger picture rather than detailing individual articles. 
+3. **Output Format**: Present your summary in JSON format with two attributes: "title" and "content". The "title" should be a brief descriptor of the summary, while the "content" should contain the synthesized overview using clean Markdown formatting.
 
-4. **Maintain Brevity**: Your summary should be concise, ideally between 800-1000 words. Use clear and direct language to ensure it is easily digestible for a quick read.
+4. **Avoid XML Tags**: Ensure that your output does not contain any XML tags, only the specified JSON format.
 
-5. **Output Format**: Present your summary in JSON format with two attributes: "title" and "content". The "title" should be a brief descriptor of the summary, while the "content" should contain the synthesized overview using clean Markdown formatting.
-
-6. **Avoid XML Tags**: Ensure that your output does not contain any XML tags, only the specified JSON format.
-
+Next, you'll receive other prompt to follow. But instructions above should always be followed.
+{instruction}
 </instruction>
-
 <examples>
 <example>
 <Input>
@@ -93,4 +90,10 @@ You are tasked with synthesizing multiple news articles into a single, concise s
 </example>
 </examples>
 ```
+"""
+
+DEFAULT_INSTRUCTION_PROMPT = """
+1. **Identify Core Themes**: As you read through the articles, look for common themes, major updates, and significant points of consensus or disagreement across the articles. 
+
+2. **Synthesize Information**: Combine the insights from all articles into one integrated summary. Focus on the bigger picture rather than detailing individual articles. 
 """
